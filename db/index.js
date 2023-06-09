c298eba4208bfff33e1452ed4063f6497d6f8d08
Elliot <ellthrowaway2@icloud.com>
2023-06-09 09:05:57 -0400

    added more boilerplate JS

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/dadsDatabase")
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("connection error", error.message);
+  });
+mongoose.set("debug", true);
+const db = mongoose.connection;
+module.exports = db;
