d204eec9c39a14108e8371f90d4f3f8887b3de22
Elliot <ellthrowaway2@icloud.com>
2023-06-12 10:34:42 -0400

    added setlist data

--- seed/artists.js
+++ seed/artists.js
@@ -9,17 +9,22 @@ const main = async (req, res) => {
         {
             artist: 'The National',
             isHeadliner: true,
-            image: '',
+            image: `/images/Matt-Berninger-All-Points-East-Aug-2022.jpeg`,
         },
         {
-            artist: ,
+            artist: 'Daughter',
             isHeadliner: false,
-            image: '',
+            image: `https://www.icmp.ac.uk/sites/default/files/styles/news_details/public/daughter-band_0.jpg?itok=AE3gFS0U`,
         },
         {
-            artist: ,
-            isHeadliner: ,
-            image: ,
+            artist: 'The Beths',
+            isHeadliner: false,
+            image: `https://www.flickr.com/photos/davidjlee/52697057674/`,
+        },
+        {
+            artist: 'Patti Smith',
+            isHeadliner: false,
+            image: `https://upload.wikimedia.org/wikipedia/commons/5/55/Patti_Smith%2C_1978.jpg`,
         },
         { timestamps: true }
     ]
@@ -29,41 +34,4 @@ const main = async (req, res) => {
 
 
 
-
-// ----------- SET LISTS ------------ //
-
-// OCT 6 2017 FOREST HILLS:
-
-// Nobody Else Will Be There
-// The System Only Dreams in Total Darkness
-// Walk It Back
-// Guilty Party
-// Don't Swallow the Cap
-// Afraid of Everyone
-// Dark Side of the Gym
-// I'll Still Destroy You
-// Turtleneck
-// I Need My Girl
-// Secret Meeting
-// Conversation 16
-// Slow Show
-// Apartment Story
-// Carin at the Liquor Store
-// Bloodbuzz Ohio
-// Day I Die
-// Fake Empire
-// About Today
-// ENCORE -------
-// Pink Rabbits
-// The KKK Took My Baby Away  
-// Mr. November
-// Terrible Love
-// Vanderlyle Crybaby Geeks
-
-
-// SEP 29 2018 FOREST HILLS
-
-// SEP 30 2018 FOREST HILLS
-
-// SEP 25 SOUND ON SOUND
 
