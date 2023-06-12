5303625c681b0a0775c33e1d3d0a30f1455c3f91
Elliot <ellthrowaway2@icloud.com>
2023-06-12 11:55:07 -0400

    more CSS work

--- client/script.js
+++ client/script.js
@@ -1,4 +1,16 @@
 
+const venue1 = document.querySelector("#venue1")
+const venue2 = document.querySelector("#venue2")
+const venue3 = document.querySelector("#venue3")
+const venue4 = document.querySelector("#venue4")
+
+venue1.innerHTML = `Forest Hills Stadium`
+venue2.innerHTML = `Seaside Park`
+venue3.innerHTML = `The Met Philadelphia`
+venue4.innerHTML = `Westville Music Bowl`
+
+
+
 
 
 const currentYear = new Date().getFullYear();
