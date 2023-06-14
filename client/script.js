e73809fa0968ebb6ca3e929a155bb8eb816c90f1
Elliot <ellthrowaway2@icloud.com>
2023-06-14 09:37:50 -0400

    installed morgan. committing before making further changes

--- client/script.js
+++ client/script.js
@@ -4,10 +4,10 @@ const baseUrl = `http://127.0.0.1:5500/client`
 const concertsBtn = document.querySelector('#concerts')
 
 // VENUES
-const venue1 = document.querySelector("#venue1")
-const venue2 = document.querySelector("#venue2")
-const venue3 = document.querySelector("#venue3")
-const venue4 = document.querySelector("#venue4")
+const venue1 = document.querySelector('#venue1')
+const venue2 = document.querySelector('#venue2')
+const venue3 = document.querySelector('#venue3')
+const venue4 = document.querySelector('#venue4')
 
 // CONCERTS
 const firstConcert = document.querySelector('#concert1')
@@ -17,20 +17,18 @@ const fourthConcert = document.querySelector('#concert4')
 const fifthConcert = document.querySelector('#concert5')
 const sixthConcert = document.querySelector('#concert6')
 
-
 // FUNCTIONS
 concertsBtn.addEventListener('click', async () => {
-    let response = await axios.get(`${apiUrl}/concerts`)
-    let concert1 = response.data.collection.concerts[0]
-    // firstConcert.innerHTML = `${concert1}`
-    console.log(concert1)
+  let response = await axios.get(`${apiUrl}/concerts`)
+  let concert1 = response.data.collection.concerts[0]
+  // firstConcert.innerHTML = `${concert1}`
+  console.log(concert1)
 })
 
-
-
-
 // FOOTER
-const currentYear = new Date().getFullYear();
-const footerDate = () => { return currentYear; }
+const currentYear = new Date().getFullYear()
+const footerDate = () => {
+  return currentYear
+}
 let copyright = document.querySelector('#year')
 copyright.innerHTML = `<p>&copy; ${currentYear} Elliot Hill</p>`
