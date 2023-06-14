eec8ab5074f1b9daa069b71d730d99bc5f7c702b
Elliot <ellthrowaway2@icloud.com>
2023-06-14 15:49:12 -0400

    event listeners for each menu button functional; container div hidden until click; each button displays corresponding data

--- client/script.js
+++ client/script.js
@@ -2,12 +2,15 @@ const apiUrl = `http://localhost:3001/api`
 const baseUrl = `http://127.0.0.1:5500/client`
 const mainContainer = document.querySelector('.container')
 const mainContent = document.querySelector('.main-content')
-// menu buttons
-const concertBtn = document.querySelector('#concerts')
+const concertsBtn = document.querySelector('#concerts')
 const venuesBtn = document.querySelector('#venues')
 const artistsBtn = document.querySelector('#artists')
 
+document.addEventListener('DOMContentLoaded', (event) => mainContainer.style.visibility = 'hidden')
 
+showContent = () => mainContainer.style.visibility = 'visible'
+
+// menu buttons
 venuesBtn.addEventListener('click', async () => {
     let response = await axios.get(`${apiUrl}/venues`)
     const venuesArr = response.data.venues
@@ -15,11 +18,39 @@ venuesBtn.addEventListener('click', async () => {
     let venuesHTML = ''
     venuesArr.forEach((venue) => {
         let venueName = venue.venue_name
-        console.log(venueName)
         venuesList += `<li class="venues">${venueName}</li>`
         venuesHTML = `<ul class="venues">${venuesList}</ul>`
     })
+    showContent()
     mainContent.innerHTML = venuesHTML
+})
+
+concertsBtn.addEventListener('click', async () => {
+    let response = await axios.get(`${apiUrl}/concerts`)
+    const concertsArr = response.data.concerts
+    let concertsList = ''
+    let concertsHTML = ''
+    concertsArr.forEach((concert) => {
+        let concertDay = concert.concert_day
+        concertsList += `<li class="concerts">${concertDay}</li>`
+        concertsHTML = `<ul class="concerts">${concertsList}</ul>`
+    })
+    showContent()
+    mainContent.innerHTML = concertsHTML
+})
+
+artistsBtn.addEventListener('click', async () => {
+    let response = await axios.get(`${apiUrl}/artists`)
+    const artistsArr = response.data.artists
+    let artistList = ''
+    let artistsHTML = ''
+    artistsArr.forEach((artist) => {
+        let artistName = artist.artist
+        artistList += `<li class="artists">${artistName}</li>`
+        artistsHTML = `<ul class="artists">${artistList}</ul`
+    })
+    showContent()
+    mainContent.innerHTML = artistsHTML
 })
 
 
@@ -36,51 +67,10 @@ venuesBtn.addEventListener('click', async () => {
 
 
 
-
-
-// const getVenues = async () => {
-//     let response = await axios.get(`${apiUrl}/venues`)
-//     const venuesArr = response.data.venues
-//     let venuesHTML = ''
-//     venuesArr.forEach((venue) => {
-//         let venueName = venue.venue_name
-//         console.log(venueName)
-//         venuesHTML += `<li class="venues">${venueName}</li>`
-//     })
-//     venueList.innerHTML = venuesHTML
-// }
-
-// venueMenu.addEventListener('click', async (event) => {
-//     event.preventDefault()
-//     console.log('venue menu clicked')
-//     getVenues()
-// })
-
-// const concertsBtn = document.querySelector('#concertsLink')
-
-// VENUES
-
-
-// CONCERTS
-// const firstConcert = document.querySelector('#concert1')
-// const secondConcert = document.querySelector('#concert2')
-// const thirdConcert = document.querySelector('#concert3')
-// const fourthConcert = document.querySelector('#concert4')
-// const fifthConcert = document.querySelector('#concert5')
-// const sixthConcert = document.querySelector('#concert6')
-
-// FUNCTIONS
-// concertsBtn.addEventListener('click', async () => {
-//   let response = await axios.get(`${apiUrl}/concerts`)
-//   let concert1 = response.data.collection.concerts[0]
-//   firstConcert.innerHTML = `${concert1}`
-//   console.log(concert1)
-// })
-
 // FOOTER
-// const currentYear = new Date().getFullYear()
-// const footerDate = () => {
-//   return currentYear
-// }
-// let copyright = document.querySelector('#year')
-// copyright.innerHTML = `<p>&copy; ${currentYear} Elliot Hill</p>`
+const currentYear = new Date().getFullYear()
+const footerDate = () => {
+  return currentYear
+}
+let copyright = document.querySelector('#year')
+copyright.innerHTML = `<p>&copy; ${currentYear} Elliot Hill</p>`
