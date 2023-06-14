a0238e9184b5cc4a1906a52cf6e0c866fc7cfd22
Elliot <ellthrowaway2@icloud.com>
2023-06-14 14:48:42 -0400

    more progress with DOM & axios calls

--- client/script.js
+++ client/script.js
@@ -1,41 +1,38 @@
 const apiUrl = `http://localhost:3001/api`
 const baseUrl = `http://127.0.0.1:5500/client`
-const venueList = document.querySelector('.venue-list')
+const venueMenu = document.querySelector('#venues')
+const venueList = document.querySelector('.venues')
 
 const getVenues = async () => {
-    // get data via axios
     let response = await axios.get(`${apiUrl}/venues`)
-    // log the data to make sure it's correct
-    // console.log(response)
     const venuesArr = response.data.venues
     let venuesHTML = ''
     venuesArr.forEach((venue) => {
         let venueName = venue.venue_name
         console.log(venueName)
         venuesHTML += `<li class="venues">${venueName}</li>`
-        // render data on screen
     })
     venueList.innerHTML = venuesHTML
-
 }
 
-
+venueMenu.addEventListener('click', async (event) => {
+    event.preventDefault()
+    console.log('venue menu clicked')
+    getVenues()
+})
 
 // const concertsBtn = document.querySelector('#concertsLink')
 
 // VENUES
-const venue1 = document.querySelector('#venue1')
-const venue2 = document.querySelector('#venue2')
-const venue3 = document.querySelector('#venue3')
-const venue4 = document.querySelector('#venue4')
+
 
 // CONCERTS
-const firstConcert = document.querySelector('#concert1')
-const secondConcert = document.querySelector('#concert2')
-const thirdConcert = document.querySelector('#concert3')
-const fourthConcert = document.querySelector('#concert4')
-const fifthConcert = document.querySelector('#concert5')
-const sixthConcert = document.querySelector('#concert6')
+// const firstConcert = document.querySelector('#concert1')
+// const secondConcert = document.querySelector('#concert2')
+// const thirdConcert = document.querySelector('#concert3')
+// const fourthConcert = document.querySelector('#concert4')
+// const fifthConcert = document.querySelector('#concert5')
+// const sixthConcert = document.querySelector('#concert6')
 
 // FUNCTIONS
 // concertsBtn.addEventListener('click', async () => {
@@ -46,9 +43,9 @@ const sixthConcert = document.querySelector('#concert6')
 // })
 
 // FOOTER
-const currentYear = new Date().getFullYear()
-const footerDate = () => {
-  return currentYear
-}
-let copyright = document.querySelector('#year')
-copyright.innerHTML = `<p>&copy; ${currentYear} Elliot Hill</p>`
+// const currentYear = new Date().getFullYear()
+// const footerDate = () => {
+//   return currentYear
+// }
+// let copyright = document.querySelector('#year')
+// copyright.innerHTML = `<p>&copy; ${currentYear} Elliot Hill</p>`
