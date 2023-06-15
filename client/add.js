7c9a641887cd44c7bcfa886ebf0fd6f37c1dc3d0
Elliot <ellthrowaway2@icloud.com>
2023-06-15 11:04:14 -0400

    created separate page for submission form

---
+++ client/add.js
@@ -0,0 +1,10 @@
+const apiUrl = `http://localhost:3001/api`
+const mainContainer = document.querySelector('.container')
+const mainContent = document.querySelector('.main-content')
+const concertsBtn = document.querySelector('#concerts')
+const venuesBtn = document.querySelector('#venues')
+const artistsBtn = document.querySelector('#artists')
+const sadDadsHome = document.querySelector('#landing')
+const heroImage = '/images/matt-berninger-sep-2018-2-3.png'
+// document.addEventListener('DOMContentLoaded', (event) => mainContainer.style.visibility = 'hidden')
+// showContent = () => mainContainer.style.visibility = 'visible'
