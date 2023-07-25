dc07a9dacbb58997c9e588605a9f4d01704cbaa1
Elliot <ellthrowaway2@icloud.com>
2023-07-25 13:16:01 -0400

    updates for deployment

--- db/index.js
+++ db/index.js
@@ -10,3 +10,14 @@ mongoose
 mongoose.set("debug", true)
 const db = mongoose.connection
 module.exports = db
+
+import mongodb from 'mongodb';
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://elliotvh:VbJBD32N1boGe5O3@saddads.tys81lo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("saddads").collection("concerts");
+  // perform actions on the collection object
+  client.close();
+});
