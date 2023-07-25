0a11fc81e325d31e5a07f923e616ec9573ae101b
Elliot <ellthrowaway2@icloud.com>
2023-07-25 14:10:58 -0400

    updated listen port

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 mongoose
-  .connect("mongodb://127.0.0.1:27017/dadsDatabase")
+  .connect("mongodb+srv://elliotvh:VbJBD32N1boGe5O3@saddads.tys81lo.mongodb.net/?retryWrites=true&w=majority")
+  // .connect("mongodb://127.0.0.1:27017/dadsDatabase")
   .then(() => {
     console.log("Successfully connected to MongoDB")
   })
@@ -11,13 +12,13 @@ mongoose.set("debug", true)
 const db = mongoose.connection
 module.exports = db
 
-import mongodb from 'mongodb';
+// import mongodb from 'mongodb';
 
-const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://elliotvh:VbJBD32N1boGe5O3@saddads.tys81lo.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("saddads").collection("concerts");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = mongodb.MongoClient;
+// const uri = "mongodb+srv://elliotvh:VbJBD32N1boGe5O3@saddads.tys81lo.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("saddads").collection("concerts");
+//   // perform actions on the collection object
+//   client.close();
+// });
